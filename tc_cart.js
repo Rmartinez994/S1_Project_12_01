22120a1148b9d374868195bf8088de23014d738b
Rafael Martinez <rmartinez994@west-mec.org>
2019-02-28 15:25:45 -0700

    Added arrays
    So what i did today was add arrays for the pricing of the sports collectors items

---
+++ tc_cart.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 1
+
+   Author: Rafael Martinez
+   Date: 2/28/19  
+   
+   Filename: tc_cart.js
+	
+*/
+var orderTotal = 0;
+var cartHTML = "<table><tr><th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th></tr>";
